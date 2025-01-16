287896d3a16894e7e93f432befb45cc4a2b6dea2
Matheus Pasti <matheusspasti@gmail.com>
2025-01-16 00:06:42 -0300

    starting project and installing shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu, Ubuntu_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ubuntuSans = Ubuntu_Sans({
+  variable: "--font-ubuntu-mono",
   subsets: ["latin"],
 });
 
@@ -23,10 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-BR">
+      <body className={cn(ubuntu.variable, ubuntuSans.variable)}>
         {children}
       </body>
     </html>
